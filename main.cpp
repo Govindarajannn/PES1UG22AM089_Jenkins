429023f52ba65737a4947e035a73a50efd8ff519
Govindarajan M <157391064+Govindarajannn@users.noreply.github.com>
2025-03-14 09:51:37 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "This is for pipeline code\n";
+  return 0;
+}
